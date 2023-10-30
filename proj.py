98be2051dbfe33c7a42d93ad9d941f054dde53bf
KazukiKenshi <hiteshd258@gmail.com>
2023-10-30 16:16:04 +0530

    Python code added

---
+++ proj.py
@@ -0,0 +1,32 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import csv
+
+bot = ChatBot(
+    'Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.BestMatch',
+        'chatterbot.logic.TimeLogicAdapter'
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+
+'''trainer = ListTrainer(bot)
+
+with open("Chatbot/datasets/1.csv",mode = 'r',encoding='utf-8') as dataset:
+
+    reader = csv.reader(dataset)
+
+    for row in reader:
+
+        trainer.train(row)
+'''
+while True:
+    try:
+        bot_input = bot.get_response(input())
+        print(bot_input)
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
